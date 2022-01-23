16e31b02f92eb4274bbf69172ec16e27ef1d5e8a
mfykmn <mfykmn@gmail.com>
2022-01-24 00:30:25 +0900

    Updated

---
+++ workspaces/test-securityhub/elb-logging-enabled/README.md
@@ -0,0 +1,12 @@
+# ELB Logging Enabled
+[AWS Security Hub ELB5](https://docs.aws.amazon.com/securityhub/latest/userguide/securityhub-standards-fsbp-controls.html#fsbp-elb-5) の解決策
+
+## 使用方法
+1. 環境構築
+    ```bash
+    $ terraform apply
+    ```
+
+## 参考リンク
+* https://dev.classmethod.jp/articles/alb-log-s3-default-encryption/
+* https://docs.aws.amazon.com/ja_jp/elasticloadbalancing/latest/classic/enable-access-logs.html#attach-bucket-policy
---
+++ workspaces/test-securityhub/elb-logging-enabled/main.tf
@@ -0,0 +1,77 @@
+####################
+# VPC
+####################
+data "aws_vpc" "default" {
+  default = true
+}
+
+variable availability_zones {
+  type = map(string)
+  default = {
+    0 = "ap-northeast-1a",
+    1 = "ap-northeast-1c",
+  }
+}
+
+resource "aws_subnet" "main" {
+  count = 2
+
+  availability_zone = var.availability_zones[count.index]
+  vpc_id            = data.aws_vpc.default.id
+  cidr_block        = cidrsubnet(data.aws_vpc.default.cidr_block, 8, count.index)
+}
+
+####################
+# S3
+####################
+resource "aws_s3_bucket" "alb_logs" {
+  acl    = "private"
+
+  # S3バケットのデフォルト暗号化
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+
+data "aws_elb_service_account" "main" {}
+
+data "aws_iam_policy_document" "alb_logs" {
+  statement {
+    effect    = "Allow"
+    actions   = [ "s3:PutObject" ]
+    resources = [ "${aws_s3_bucket.alb_logs.arn}/*" ]
+    principals {
+      type = "AWS"
+      identifiers = [ data.aws_elb_service_account.main.arn ]
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "alb_logs" {
+  bucket = aws_s3_bucket.alb_logs.id
+  policy = data.aws_iam_policy_document.alb_logs.json
+}
+
+####################
+# ALB
+####################
+resource "aws_default_security_group" "default" {
+  vpc_id = data.aws_vpc.default.id
+}
+
+resource "aws_alb" "alb" {
+  name                       = "elb-logging-enabled"
+  security_groups            = [aws_default_security_group.default.id]
+  subnets                    = [aws_subnet.main[0].id, aws_subnet.main[1].id]
+  internal                   = false
+  enable_deletion_protection = false
+  access_logs {
+    bucket  = aws_s3_bucket.alb_logs.bucket
+    prefix  = "PREFIX"
+    enabled = true
+  }
+}
